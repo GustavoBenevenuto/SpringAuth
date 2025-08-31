8810315f70acfdc891e4967215f61227cfe61d29
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2025-08-31 13:33:33 -0300

    Update repository

--- src/main/java/com/springauth/domain/products/repository/ProductRepository.java
+++ src/main/java/com/springauth/domain/products/repository/ProductRepository.java
@@ -1,10 +1,11 @@
 package com.springauth.domain.products.repository;
 
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.springauth.domain.products.entity.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, String> {
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
--- src/main/java/com/springauth/domain/users/repository/UserRepository.java
+++ src/main/java/com/springauth/domain/users/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.springauth.domain.users.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.springauth.domain.users.entity.User;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 	
 	UserDetails findByEmail(String email);
 }
